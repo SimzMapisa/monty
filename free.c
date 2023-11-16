4d5da7715fd869ee0ef99adb75bb149a760bb6db
SimzMapisa <simbarashe795@gmail.com>
2023-11-16 16:49:24 +0200

    fix betty tabs erros

--- free.c
+++ free.c
@@ -8,12 +8,11 @@
 
 void free_stack(stack_t **stack)
 {
-    stack_t *tmp;
-
-    while (*stack != NULL)
-    {
-        tmp = *stack;
-        *stack = (*stack)->next;
-        free(tmp);
-    }
+	stack_t *tmp;
+	while (*stack != NULL)
+	{
+		tmp = *stack;
+		*stack = (*stack)->next;
+		free(tmp);
+	}
 }
